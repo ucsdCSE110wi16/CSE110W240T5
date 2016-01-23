a30b9155e69957624ebed50b3556dba2f96c8fcf
shunge <gyishun@gmail.com>
2016-01-22 21:54:24 -0800

    A demo

---
+++ APISAMPLE/app/src/main/java/course/hello/shaun/apisample/MainActivity.java
@@ -0,0 +1,155 @@
+package course.hello.shaun.apisample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.widget.Toast;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "APItestingSample";
+
+    public final static String Name = "course.hello.shaun.Demo.Name";
+    public final static String Add = "course.hello.shaun.Demo.Add";
+    public final static String Rate = "course.hello.shaun.Demo.Rate";
+    public final static String Price = "course.hello.shaun.Demo.Price";
+
+    /**
+     * Request code passed to the PlacePicker intent to identify its result when it returns.
+     */
+    private static final int REQUEST_PLACE_PICKER = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showPlaces(View V) {
+
+            // BEGIN_INCLUDE(intent)
+            /* Use the PlacePicker Builder to construct an Intent.
+            Note: This sample demonstrates a basic use case.
+            The PlacePicker Builder supports additional properties such as search bounds.
+             */
+            try {
+                PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
+                Intent intent = intentBuilder.build(this);
+                // Start the Intent by requesting a result, identified by a request code.
+                startActivityForResult(intent, REQUEST_PLACE_PICKER);
+
+                // Hide the pick option in the UI to prevent users from starting the picker
+                // multiple times.
+
+            } catch (GooglePlayServicesRepairableException e) {
+                GooglePlayServicesUtil
+                        .getErrorDialog(e.getConnectionStatusCode(), this, 0);
+            } catch (GooglePlayServicesNotAvailableException e) {
+                Toast.makeText(this, "Google Play Services is not available.",
+                        Toast.LENGTH_LONG)
+                        .show();
+            }
+
+            // END_INCLUDE(intent)
+        }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // BEGIN_INCLUDE(activity_result)
+        if (requestCode == REQUEST_PLACE_PICKER) {
+            // This result is from the PlacePicker dialog.
+
+
+            if (resultCode == Activity.RESULT_OK) {
+                /* User has picked a place, extract data.
+                   Data is extracted from the returned intent by retrieving a Place object from
+                   the PlacePicker.
+                 */
+                final Place place = PlacePicker.getPlace(data, this);
+
+                /* A Place object contains details about that place, such as its name, address
+                and phone number. Extract the name, address, phone number, place ID and place types.
+                 */
+                final CharSequence name = place.getName();
+                final CharSequence address = place.getAddress();
+                final CharSequence phone = place.getPhoneNumber();
+                final float rating = place.getRating();
+                final String placeId = place.getId();
+                String attribution = PlacePicker.getAttributions(data);
+                if(attribution == null){
+                    attribution = "";
+                }
+
+
+                Intent intent = new Intent(this, DisplayPlacesActivity.class);
+
+                intent.putExtra(Name,name);
+                intent.putExtra(Add, address);
+                intent.putExtra(Rate, rating);
+                startActivity(intent);
+
+
+                // Print data to debug log
+                Log.d(TAG, "Place selected: " + placeId + " (" + name.toString() + ")");
+
+
+            } else {
+
+                Log.d(TAG, "Nothing man.");
+            }
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+        // END_INCLUDE(activity_result)
+    }
+
+
+
+}
