b38372572237389c7066fc32d5e44153cd07fd26
SkyLoveWing <skyliang1314@gmail.com>
2016-01-31 17:02:46 -0800

    UI designed added

---
+++ UI/DemoApp/app/src/main/java/com/xu/shawn/demoapp/RestInfoActivity.java
@@ -0,0 +1,39 @@
+package com.xu.shawn.demoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class RestInfoActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnGoback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rest_info);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btnGoback = (Button)findViewById(R.id.Goback);
+        btnGoback.setOnClickListener(this);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, PreferenceActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ UI/DemoApp2/app/src/main/java/com/xu/shawn/demoapp/ProfileActivity.java
@@ -0,0 +1,38 @@
+package com.xu.shawn.demoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnGoPre;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btnGoPre = (Button)findViewById(R.id.GotoPre);
+        btnGoPre.setOnClickListener(this);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, PreferenceActivity.class);
+        startActivity(intent);
+    }
+}
