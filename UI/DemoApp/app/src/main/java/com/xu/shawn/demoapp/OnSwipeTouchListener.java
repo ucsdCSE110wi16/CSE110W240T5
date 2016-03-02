b8c3439af9ab7894ad9a18c1af5dcda9eca170ab
shunge <gyishun@gmail.com>
2016-03-02 11:02:31 -0800

    Revert "UI"
    This reverts commit 7f844a2d2959bf9d0d93088ac9399b8e4fa285b6.

--- UI/DemoApp/app/src/main/java/com/xu/shawn/demoapp/OnSwipeTouchListener.java
+++ UI/DemoApp/app/src/main/java/com/xu/shawn/demoapp/OnSwipeTouchListener.java
@@ -34,8 +34,8 @@ public class OnSwipeTouchListener implements OnTouchListener {
 
     private final class GestureListener extends SimpleOnGestureListener {
 
-        private static final int SWIPE_THRESHOLD = 50;
-        private static final int SWIPE_VELOCITY_THRESHOLD = 50;
+        private static final int SWIPE_THRESHOLD = 100;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
 
         @Override
         public boolean onDown(MotionEvent e) {
